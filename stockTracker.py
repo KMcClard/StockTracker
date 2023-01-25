8274e51b4b3d2fbcbd637dd75450be70bbc0289b
KMcClard <93171143+KMcClard@users.noreply.github.com>
2023-01-25 16:33:34 +0000

    bleh

---
+++ stockTracker.py
@@ -0,0 +1,7 @@
+import yfinance as yf
+ticker = yf.Ticker('GOOGL').info
+market_price = ticker['regularMarketPrice']
+previous_close_price = ticker['regularMarketPreviousClose']
+print('Ticker: GOOGL')
+print('Market Price:', market_price)
+print('Previous Close Price:', previous_close_price)
